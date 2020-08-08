4ea66cfa82bd5bd9242579460f2c4458bb4a593b
Bartosz Lis <bartoszlis87@gmail.com>
2020-08-08 22:07:26 +0200

    forEach, map, filter, sort

--- array/app.js
+++ array/app.js
@@ -16,15 +16,15 @@ const sum = [...alfa, ...beta];
 console.log('Suma elementów w tablicy: ', sum);
 
 //sprawdzanie czy obie tablice sa takie same(przez referencje)
-if(alfa === beta){
+if (alfa === beta) {
     console.log(true)
-}else{
+} else {
     console.log(false)
 }
 console.log('*----------*');
 //Pętla forEach
-fruits = ['banana','apple', 'raspberry' ];
-fruits.forEach(function(item, index, array) {
+fruits = ['banana', 'apple', 'raspberry'];
+fruits.forEach(function (item, index, array) {
     console.log('Elementy tablicy: ', [item, index]);
     console.log('Tablica: ', fruits);
 });
@@ -41,7 +41,7 @@ console.log(firstElement);
 // Banana
 console.log('*----------*');
 
-//* Sprwdzanie ostatniego elementu tablicy
+//* Sprawdzanie ostatniego elementu tablicy
 let lastElement = fruits[fruits.length - 1];
 console.log(lastElement);
 // raspberry
@@ -114,9 +114,63 @@ try {
 
         sum1 = sum1 + alfaSum[i];
     }
-}catch (err){
+} catch (err) {
     console.log(err);
 }
-console.log('Tablica: ',alfaSum, 'Suma tablicy: ', sum1);
+console.log('Tablica: ', alfaSum, 'Suma tablicy: ', sum1);
 console.log('*----------*');
 //metoda forEach()
+
+let betaSum = [123, 22, 44, 33];
+let sum2 = 0;
+betaSum.forEach(function (num) {
+    sum2 += num;
+});
+console.log('Suma :', sum2);
+
+console.log('*----------SORTOWANIE-----*');
+// od największej do najmniejszej
+let alfaSort = [11, 23, 4, 55, 678, 1];
+console.log('Nie posortowana tablica: ', alfaSort);
+alfaSort.sort((a, b) => {
+    return b - a;
+});
+console.log('Posortowana tablica: ', alfaSort);
+//Najmniejsej do największej
+alfaSort.sort((a, b) => {
+    return a-b;
+});
+console.log('Posortowana tablica od najmniejszej do największej: ', alfaSort);
+//Sort porównanie długości elementów w tablicy
+let arraySort = [`a`, 'fffed', 'wew', 'werwer'];
+arraySort.sort((a , b) => {
+    return b.length - a.length;
+});
+console.log(arraySort);
+console.log('*----------HTMLCollection*');
+//pobieranie elementów z html i zapisywanie do zmiennej
+const pList = document.getElementsByTagName('p');
+console.log(pList);
+console.log('*----------Pętla FOR*');
+for (let i = 0; i < pList.length; i++) {
+    const item = pList[i]
+    item.style.color = 'red';
+}
+console.log('*----------Pętla forEach*');
+const pListArr = [...pList];
+pListArr.forEach((el) => {
+    el.style.color = 'blue';
+});
+console.log('*----------MAP*');
+const alfaMap = ['aaa', 'bb', 'cccc'];
+const alfaMapArr = alfaMap.map((item) => {
+    return item + '11'
+});
+console.log(alfaMapArr);
+console.log('*----------Tablica 2 wymiarowa*');
+// const cart = [
+//         ['skarpetki', 3, 4.90]
+//         ['gacie', 8, 20.00]
+//         ['top', 4, 19.90]
+// ];
+// console.log(cart[1][2]);
